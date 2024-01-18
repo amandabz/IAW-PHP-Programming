7ffe8a87c304c09bf6598852a24135aab61da7ed
amandabz <amandabenitezhidalgo@gmail.com>
2024-01-18 14:37:57 +0100

    login and close session created

--- loginNoDB/contenido.php
+++ loginNoDB/contenido.php
@@ -1,4 +1,22 @@
 <?php
-echo "Has inciado sesión correctamente.";
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <?php
+    if(isset($_SESSION["username"])) {
+        $username = $_SESSION["username"];
+        echo "Has inciado sesión correctamente con el usuario: $username";
+    } else {
+        header("Location: login.php");
+    }
+    ?>
+    <br>
+    <br>
+    <a href="cerrarsesion.php">Cerrar sesión</a>
+</body>
+<html lang="en">
 
 
