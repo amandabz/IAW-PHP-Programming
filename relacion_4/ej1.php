85e594be7eb5fd99af83991ce7c8456dca5d7839
amandabz <amandabenitezhidalgo@gmail.com>
2024-01-24 14:35:03 +0100

    list of exercises 4: ex1 (only 'esPrimo' function

--- relacion_4/ej1.php
+++ relacion_4/ej1.php
@@ -4,5 +4,27 @@
 // a. esCapicua: Devuelve verdadero si el número que se pasa como parámetro es capicúa y falso en caso contrario.
 // b. esPrimo: Devuelve verdadero si el número que se pasa como parámetro es primo y falso en caso contrario.
 
+$n = readline("Escribe un numero para comprobar si es primo o no: ");
 
+function esPrimo($numero) {
+    $esPrimo = true;
+
+    for ($i = 2; $i < $numero; $i++) {
+        if ($numero % $i == 0) {
+            $esPrimo = false;
+            break;
+        }
+    }
+    if ($numero == 0 || $numero == 1) {
+        $esPrimo = false;
+    }
+
+    return $esPrimo;
+}
+
+if (esPrimo($n)) {
+    echo "El $n es primo.\n";
+} else {
+    echo "El $n no es primo.\n";
+}
 
